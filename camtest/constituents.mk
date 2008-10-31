06123eceee834310656a8ee002ec701d6036fb42
weaver <none@example.com>
2008-10-31 17:30:48 +0000

    Fix for external leutron package

--- camtest/constituents.mk
+++ camtest/constituents.mk
@@ -1,17 +1,9 @@
-#
-#  Only build for i386-linux.  (rdcds211)
-#
-ifneq ($(findstring i386-linux-,$(tgt_arch)),)
-
 libnames := 
  
-#libsrcs_test := EventTest.cc EventOptions.cc
-
 tgtnames          := camtest
 
 tgtsrcs_camtest := camtest.cc FrameServer.cc
 tgtlibs_camtest := pds/service pds/collection pds/xtc pds/utility pds/management pds/client pds/camera
-tgtslib_camtest := /usr/lib/lvsds
-tgtsinc_camtest := /usr/include/lvsds
-
-endif
+tgtlibs_camtest += leutron/lvsds
+tgtlibs_camtest += leutron/LvSerialCommunication
+tgtincs_camtest := leutron/include
