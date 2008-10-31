84018089525d129412da40dfe839e151727d2837
cpo <none@example.com>
2008-10-31 17:51:22 +0000

    support external acqiris/evgr packages

--- devtest/constituents.mk
+++ devtest/constituents.mk
@@ -3,9 +3,11 @@ CPPFLAGS += -D_ACQIRIS -D_LINUX
 tgtnames          := evgrtest acqtest
 
 tgtsrcs_acqtest := acqtest.cc
+tgtincs_acqtest := acqiris evgr
 tgtlibs_acqtest := acqiris/AqDrv4 evgr/evr evgr/evg pds/service pds/acqiris pds/collection pds/xtc pds/utility pds/management pds/client pds/evgr
 tgtslib_acqtest := /usr/lib/rt
 
 tgtsrcs_evgrtest := evgrtest.cc
+tgtincs_evgrtest := evgr
 tgtlibs_evgrtest := evgr/evg evgr/evr pds/service pds/collection pds/xtc pds/utility pds/management pds/client pds/evgr
 tgtslib_evgrtest := /usr/lib/rt
