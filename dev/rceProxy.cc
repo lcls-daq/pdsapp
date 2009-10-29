48b187725887450c73af43ec77b06ed10e9d3c3c
tomytsai <none@example.com>
2009-10-29 17:18:42 +0000

    Option bug

--- dev/rceProxy.cc
+++ dev/rceProxy.cc
@@ -194,7 +194,7 @@ int main(int argc, char** argv)
             
         switch(opt) 
         {            
-        case 's':
+        case 'x':
             showVersion();
             return 0;            
         case 'h':               
