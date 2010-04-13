ff66a2f0766f36dd897dd371c55146eb86ac2c54
jtiller <none@example.com>
2010-04-13 23:19:00 +0000

    Fixed the problem with my enumerated types.  Used an actual enum type for the count_mode and quad_mode.  Based the boolean enumeration on Enums::Bool (I'd seen others do that).

--- config/EncoderConfig.cc
+++ config/EncoderConfig.cc
@@ -11,19 +11,23 @@ using namespace Pds_ConfigDb;
 
 // FIXME: This is blatant duplication of code in
 // pds/encoder/driver/pci3e-wrapper.cc.  Yuck!
+//
+// These must end in NULL.
 namespace PCI3E
 {
    extern const char* count_mode_to_name[] = {
       "COUNT_MODE_WRAP_FULL",
       "COUNT_MODE_LIMIT",
       "COUNT_MODE_HALT",
-      "COUNT_MODE_WRAP_PRESET"
+      "COUNT_MODE_WRAP_PRESET",
+      NULL
    };
    extern const char* quad_mode_to_name[] = {
       "QUAD_MODE_CLOCK_DIR",
       "QUAD_MODE_X1",
       "QUAD_MODE_X2",
-      "QUAD_MODE_X4"
+      "QUAD_MODE_X4",
+      NULL
    };
 }
 
@@ -37,22 +41,30 @@ class Pds_ConfigDb::EncoderConfig::Private_Data {
    int dataSize() const
       { return sizeof(EncoderConfigType); }
 
-   NumericInt<uint8_t>  _chan_num;
-   Enumerated<uint8_t>  _count_mode;
-   Enumerated<uint8_t>  _quadrature_mode;
-   NumericInt<uint8_t>  _input_num;
-   Enumerated<uint8_t>  _input_rising;   
-   NumericInt<uint32_t> _ticks_per_sec;
+   NumericInt<uint8_t>     _chan_num;
+   Enumerated<Pds::Encoder::ConfigV1::count_mode::type_t> _count_mode;
+   Enumerated<Pds::Encoder::ConfigV1::quad_mode::type_t>  _quadrature_mode;
+   NumericInt<uint8_t>     _input_num;
+   Enumerated<Enums::Bool> _input_rising;   
+   NumericInt<uint32_t>    _ticks_per_sec;
 };
 
 Pds_ConfigDb::EncoderConfig::Private_Data::Private_Data() 
-   : _chan_num("Channel Number ", 1, 1, 3),
-     _count_mode("Counter Mode ", 0, PCI3E::count_mode_to_name ),
-     _quadrature_mode("Quadrature Mode ", 0, PCI3E::quad_mode_to_name ),
-     _input_num("Trigger Input Number ", 0, 0, 3),
-     _input_rising("Trigger on Rising Edge ", 1, Enums::Bool_Names ),
-     _ticks_per_sec("Timestamp ticks per second ",
-                    33000000, 32000000, 34000000)
+   : _chan_num(        "Channel Number ",
+                       1, 1, 3 ),
+     _count_mode(      "Counter Mode ",
+                       Pds::Encoder::ConfigV1::count_mode::WRAP_FULL,
+                       PCI3E::count_mode_to_name ),
+     _quadrature_mode( "Quadrature Mode ",
+                       Pds::Encoder::ConfigV1::quad_mode::X1,
+                       PCI3E::quad_mode_to_name ),
+     _input_num(       "Trigger Input Number ",
+                       0, 0, 3 ),
+     _input_rising(    "Trigger on Rising Edge ",
+                       Enums::True,
+                       Enums::Bool_Names ),
+     _ticks_per_sec(   "Timestamp ticks per second ",
+                       33000000, 32000000, 34000000)
 {}
 
 void Pds_ConfigDb::EncoderConfig::Private_Data::insert( Pds::LinkedList<Parameter>& pList )
@@ -73,8 +85,9 @@ int Pds_ConfigDb::EncoderConfig::Private_Data::pull( void* from )
    _count_mode.value      = encoderConf._count_mode;
    _quadrature_mode.value = encoderConf._quadrature_mode;
    _input_num.value       = encoderConf._input_num;
-   _input_rising.value    = encoderConf._input_rising;
-   _ticks_per_sec.value    = encoderConf._ticks_per_sec;
+   _input_rising.value    = encoderConf._input_rising ? Enums::True
+                                                      : Enums::False;
+   _ticks_per_sec.value   = encoderConf._ticks_per_sec;
 
    return sizeof(EncoderConfigType);
 }
@@ -85,7 +98,7 @@ int Pds_ConfigDb::EncoderConfig::Private_Data::push(void* to)
                                _count_mode.value,
                                _quadrature_mode.value,
                                _input_num.value,
-                               _input_rising.value,
+                               _input_rising.value == Enums::True,
                                _ticks_per_sec.value );
 
    return sizeof(EncoderConfigType);
