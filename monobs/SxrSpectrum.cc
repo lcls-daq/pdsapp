81f871ee09718505d1267c1e1038e77d90613064
weaver <none@example.com>
2010-05-06 22:42:39 +0000

    *** empty log message ***

--- monobs/SxrSpectrum.cc
+++ monobs/SxrSpectrum.cc
@@ -116,10 +116,11 @@ void   SxrSpectrum::_event    (const void* payload, const Pds::ClockTime& t)
   _sem.take();
   double* p = _spectrum;
   const FrameType& frame = *static_cast<const FrameType*>(payload);
+  int32_t o = frame.offset()*(xhi-xlo);
   const uint16_t* d = reinterpret_cast<const uint16_t*>(frame.data());
   d += ylo*frame.width() + xlo;
   for(unsigned y=0; y<nb; y++) {
-    int32_t v = 0;
+    int32_t v = -o;
     for(unsigned x=xlo; x<xhi; x++)
       v += *d++;
     *p++ += double(v);
