7931031802b293e9391b207ce41056a529389765
weaver <none@example.com>
2010-11-24 21:58:05 +0000

    *** empty log message ***

--- bldIpimb/constituents.mk
+++ bldIpimb/constituents.mk
@@ -9,6 +9,7 @@ tgtnames    := bldIpimb
 
 commonlibs  := pdsdata/xtcdata pdsdata/appdata
 commonlibs  += pds/service pds/collection pds/xtc pds/mon pds/vmon pds/utility pds/management pds/client pds/config 
+datalibs := pdsdata/xtcdata pdsdata/opal1kdata pdsdata/pulnixdata pdsdata/camdata pdsdata/pnccddata pdsdata/evrdata pdsdata/acqdata pdsdata/controldata pdsdata/princetondata pdsdata/ipimbdata pdsdata/encoderdata pdsdata/fccddata pdsdata/lusidata pdsdata/cspaddata
 
 #ifeq ($(shell uname -m | egrep -c '(x86_|amd)64$$'),1)
 #ARCHCODE=64
@@ -18,7 +19,8 @@ ARCHCODE=32
 
 tgtsrcs_bldIpimb := BldIpimb.cc
 tgtincs_bldIpimb := evgr
-tgtlibs_bldIpimb := $(commonlibs)  evgr/evr evgr/evg pds/evgr  pdsdata/evrdata  pdsdata/ipimbdata pds/ipimb pdsapp/bldIpimblib
+tgtlibs_bldIpimb := $(commonlibs) $(datalibs) evgr/evr evgr/evg pds/evgr pds/ipimb pdsapp/configdb pdsapp/bldIpimblib
+tgtlibs_bldIpimb += qt/QtGui qt/QtCore
 tgtslib_bldIpimb := /usr/lib/rt
 
 
