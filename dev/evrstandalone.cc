12a2d2d4894f581f2a8eba58fa5ec9945087b43e
weaver <none@example.com>
2010-12-04 00:18:57 +0000

    Change to evrstandalong commandline

--- dev/evrstandalone.cc
+++ dev/evrstandalone.cc
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "evgr/evr/evr.hh"
 #include "pds/evgr/EvgrBoardInfo.hh"
@@ -9,6 +10,24 @@
 using namespace Pds;
 
 static EvgrBoardInfo<Evr> *erInfoGlobal;
+
+class PulseParams {
+public:
+  unsigned eventcode;
+  unsigned delay;
+  unsigned width;
+  unsigned output;
+};
+
+const unsigned MAX_PULSES = 10;
+unsigned    npulses;
+PulseParams pulse[MAX_PULSES];
+
+//
+//  AMO : {41,0,11900,8}  // pnCCD clear
+//        {41,0,11900,9}  // pnCCD clear
+//        {42,0,   11,7}  // YAG laser
+//
 
 class EvrStandAloneManager {
 public:
@@ -32,49 +51,13 @@ void EvrStandAloneManager::configure() {
   int ram=0;
   _er.MapRamEnable(ram,0);
 
-  //
-  //  pnCCD clear signal
-  //
-  {
-    int opcode=41;
-    //   _er.SetFIFOEvent(ram, opcode, enable);
-    int trig=0; 
-    _er.SetPulseMap(ram, opcode, trig, -1, -1);
-    
-    int pulse = 0; int presc = 1;
-    int polarity=0;
-    _er.SetPulseProperties(pulse, polarity, 0, 0, 1, 1);
-
-    const double tickspersec = 119.0e6;
-    
-    int delay=(int)(0.0*tickspersec);
-    int width=(int)(100.e-6*tickspersec);
-    _er.SetPulseParams(pulse,presc,delay,width);
-    
-    _er.SetUnivOutMap( 8, pulse);
-    _er.SetUnivOutMap( 9, pulse);
-  }
-
-  //
-  //  YAG laser trigger
-  //
-  {
-    int opcode=42;
-    //   _er.SetFIFOEvent(ram, opcode, enable);
-    int trig=1; 
-    _er.SetPulseMap(ram, opcode, trig, -1, -1);
-    
-    int pulse = 1; int presc = 1;
-    int polarity=0;
-    _er.SetPulseProperties(pulse, polarity, 0, 0, 1, 1);
-
-    const double tickspersec = 119.0e6;
-    
-    int delay=(int)(0.0*tickspersec);
-    int width=(int)(100.e-9*tickspersec);
-    _er.SetPulseParams(pulse,presc,delay,width);
-    
-    _er.SetUnivOutMap( 7, pulse);
+  for(unsigned i=0; i<npulses; i++) {
+    printf("Configuring pulse %d  eventcode %d  delay %d  width %d  output %d\n",
+           i, pulse[i].eventcode, pulse[i].delay, pulse[i].width, pulse[i].output);
+    _er.SetPulseMap(ram, pulse[i].eventcode, 0, -1, -1);
+    _er.SetPulseProperties(i, 0, 0, 0, 1, 1);
+    _er.SetPulseParams(i,1,pulse[i].delay,pulse[i].width);
+    _er.SetUnivOutMap( pulse[i].output, i);
   }
 
   _er.IrqEnable(EVR_IRQ_MASTER_ENABLE | EVR_IRQFLAG_EVENT);
@@ -109,16 +92,34 @@ EvrStandAloneManager::EvrStandAloneManager(EvgrBoardInfo<Evr> &erInfo) :
   erInfoGlobal = &erInfo;
 }
 
+void usage(const char* p) {
+  printf("Usage: %s -r <evr a/b> -p <eventcode,delay,width,output> [-p ...]\n",p);
+  printf("\teventcode : [40=120Hz, 41=60Hz, ..]\n");
+  printf("\tdelay,width in 119MHz ticks [1=8.4ns, 2=16.8ns, ..]\n");
+  printf("\toutput : connector number [0=Univ0,..]\n");
+}
+
 int main(int argc, char** argv) {
 
   extern char* optarg;
   char* evrid=0;
+  char* endptr;
   int c;
-  while ( (c=getopt( argc, argv, "g:r:")) != EOF ) {
+  while ( (c=getopt( argc, argv, "r:p:h")) != EOF ) {
     switch(c) {
     case 'r':
       evrid  = optarg;
       break;
+    case 'p':
+      pulse[npulses].eventcode = strtoul(optarg  ,&endptr,0);
+      pulse[npulses].delay     = strtoul(endptr+1,&endptr,0);
+      pulse[npulses].width     = strtoul(endptr+1,&endptr,0);
+      pulse[npulses].output    = strtoul(endptr+1,&endptr,0);
+      npulses++;
+      break;
+    case 'h':
+      usage(argv[0]);
+      exit(1);
     }
   }
 
