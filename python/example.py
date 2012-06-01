dd9c8bac4c308b50b18adcc59d4219bf8c26bc7e
weaver <none@example.com>
2012-06-01 05:22:51 +0000

    Example configdb schema upgrade

--- python/example.py
+++ python/example.py
@@ -2,12 +2,12 @@ import pycdb
 
 def evolve(db,fname):
     y = db.get(typeid=0x00050008,src=0x100,file=fname)[0]
-    v = x.evolve(6,y)
+    v = db.evolve(6,y)
     v.set(y.get())
-    x.set(file=fname,xtc=v)
+    db.set(file=fname,xtc=v)
 
-db = pycdb.Db("/reg/lab2/home/tstopr/configdb/weaver")
-evolve(db,'cspad_v3.xtc')
-evolve(db,'live_scan.xtc')
-evolve(db,'jack_newer.xtc')
+db = pycdb.Db("/reg/g/pcds/dist/pds/cxi/configdb/current")
+evolve(db,'beam_shutter_v5.xtc')
+evolve(db,'beam_v5.xtc')
+evolve(db,'twoevr_nobeam_v5.xtc')
        
