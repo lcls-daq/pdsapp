a015897add184be1cb29781f525b3db403ffc76d
jackp <none@example.com>
2012-06-22 17:29:12 +0000

    Thu Jun 21 11:21:52 PDT 2012 jackp Added command line pulse definitions to evgr

--- devtest/evgr.cc
+++ devtest/evgr.cc
@@ -1,5 +1,8 @@
 #include "pds/evgr/EvgrBoardInfo.hh"
 #include "pds/evgr/EvgrManager.hh"
+#include "pds/evgr/EvgrPulseParams.hh"
+#include "pdsapp/config/EventcodeTiming.hh"
+#include "pdsapp/config/EventcodeTiming.cc"
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -7,13 +10,31 @@
 
 using namespace Pds;
 
+void usage(const char* p) {
+  printf("Usage: %s -r <evr a/b> -p <eventcode,delay,width,output[,polarity]> [-p ...]\n",p);
+  printf("\teventcode : [40=120Hz, 41=60Hz, ..]\n");
+  printf("\tdelay,width in 119MHz ticks [1=8.4ns, 2=16.8ns, ..]\n");
+  printf("\toutput : connector number [0=Univ0,..]\n");
+  printf("\tpolarity : 0=pos(default) | 1=neg\n");
+}
+
 int main(int argc, char** argv) {
 
+  const unsigned MAX_PULSES = 4;
+  unsigned    npulses = 0;
+  EvgrPulseParams pulse[MAX_PULSES];
+  unsigned tooMany = 0;
+
+  unsigned ticks;
+  int delta;
+  unsigned udelta;
+
   extern char* optarg;
+  char* endptr;
   char* evgid=0;
   char* evrid=0;
   int c;
-  while ( (c=getopt( argc, argv, "g:r:")) != EOF ) {
+  while ( (c=getopt( argc, argv, "g:r:p:h")) != EOF ) {
     switch(c) {
     case 'g':
       evgid = optarg;
@@ -21,6 +42,41 @@ int main(int argc, char** argv) {
     case 'r':
       evrid  = optarg;
       break;
+    case 'p':
+      if (npulses < MAX_PULSES) {
+        pulse[npulses].eventcode = strtoul(optarg  ,&endptr,0);
+        ticks = Pds_ConfigDb::EventcodeTiming::timeslot(pulse[npulses].eventcode);
+        delta = ticks - Pds_ConfigDb::EventcodeTiming::timeslot(140);
+        udelta = abs(delta);
+        pulse[npulses].delay     = strtoul(endptr+1,&endptr,0);
+        if (pulse[npulses].delay>udelta) pulse[npulses].delay -= delta;
+        pulse[npulses].width     = strtoul(endptr+1,&endptr,0);
+        pulse[npulses].output    = strtoul(endptr+1,&endptr,0);
+        if (*endptr==',') {
+          pulse[npulses].polarity = strtoul(endptr+1,&endptr,0);
+        } else {
+          pulse[npulses].polarity = 0;
+        }
+        npulses++;
+      } else {
+        unsigned a = (unsigned)strtoul(optarg  ,&endptr,0);
+        unsigned b = (unsigned)strtoul(endptr+1,&endptr,0);
+        unsigned c = (unsigned)strtoul(endptr+1,&endptr,0);
+        unsigned d = (unsigned)strtoul(endptr+1,&endptr,0);
+        printf("Pulse %u,%u,%u,%u", a, b, c, d);
+        if (*endptr==',') {
+          printf(",%u", (unsigned)strtoul(endptr+1,&endptr,0));
+        }
+        printf(" is %u too many!\n", ++tooMany);
+      }
+      break;
+    case 'h':
+      usage(argv[0]);
+      exit(0);
+    default:
+      printf("Option not understood!\n");
+      usage(argv[0]);
+      exit(1);
     }
   }
 
@@ -35,7 +91,7 @@ int main(int argc, char** argv) {
 
   EvgrBoardInfo<Evg>& egInfo = *new EvgrBoardInfo<Evg>(evgdev);
   EvgrBoardInfo<Evr>& erInfo = *new EvgrBoardInfo<Evr>(evrdev);
-  new EvgrManager(egInfo,erInfo);
+  new EvgrManager(egInfo,erInfo, npulses, pulse);
   while (1) sleep(10);
   return 0;
 }
