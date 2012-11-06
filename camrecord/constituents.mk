b7b58ce93d69fc03ba9412058f45fde2458ada95
tomytsai <none@example.com>
2012-11-06 23:51:20 +0000

    *** empty log message ***

--- camrecord/constituents.mk
+++ camrecord/constituents.mk
@@ -1,6 +1,6 @@
 tgtnames := camrecord
 
-commonlibs := pdsdata/xtcdata pdsdata/camdata pdsdata/pulnixdata pdsdata/opal1kdata pdsdata/cspaddata pdsdata/cspad2x2data pdsdata/timepixdata pdsdata/camdata
+commonlibs := pdsdata/xtcdata pdsdata/camdata pdsdata/pulnixdata pdsdata/opal1kdata pdsdata/cspaddata pdsdata/cspad2x2data pdsdata/timepixdata pdsdata/camdata pdsdata/compressdata
 tgtsrcs_camrecord := bld.cc ca.cc main.cc xtc.cc
 tgtlibs_camrecord := $(commonlibs) epics/ca epics/Com
 tgtincs_camrecord := epics/include epics/include/os/Linux
