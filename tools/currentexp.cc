11ed8d21735a1db200c95c8777df939d4054cfda
caf <none@example.com>
2012-11-08 20:18:28 +0000

    fixed offlinereader path

--- tools/currentexp.cc
+++ tools/currentexp.cc
@@ -12,7 +12,7 @@
 #define MAXLINE 255
 #define DEFAULT_STATION 0u
 
-#define DEFAULT_RCPATH  "/reg/g/pcds/dist/pds/misc/.offlinerc"
+#define DEFAULT_RCPATH  "/reg/g/pcds/dist/pds/misc/.offlinereader"
 
 #include <string>
 #include <vector>
