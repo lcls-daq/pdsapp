f85bc41847d7ad4cadd5e878cda4f18cb583ea34
caf <none@example.com>
2012-11-09 02:17:33 +0000

    offlineobs looks up current experiment

--- monobs/offlineobs.cc
+++ monobs/offlineobs.cc
@@ -46,7 +46,7 @@ private:
 };
 
 void usage(char* progname) {
-  printf("Usage: %s -p <platform> -P <partition> -E <experiment_name> -L <offlinerc> [-V <parm_list_file>]\n", progname);
+  printf("Usage: %s -p <platform> -P <partition> -L <offlinerc> [-V <parm_list_file>] [-v]\n", progname);
 }
 
 // Appliance* app;
@@ -72,13 +72,13 @@ int main(int argc, char** argv) {
 
   unsigned platform=-1UL;
   const char* partition = 0;
-  const char* experiment_name = 0;
   const char* offlinerc = 0;
   const char* parm_list_file = 0;
   unsigned nodes =  0;
+  int verbose = 0;
   (void) signal(SIGINT, sigfunc);
   int c;
-  while ((c = getopt(argc, argv, "p:P:E:L:V:")) != -1) {
+  while ((c = getopt(argc, argv, "p:P:E:L:V:v")) != -1) {
     errno = 0;
     char* endPtr;
     switch (c) {
@@ -90,7 +90,7 @@ int main(int argc, char** argv) {
       partition = optarg;
       break;
     case 'E':
-      experiment_name = optarg;
+      printf("%s: -E flag is ignored, current experiment will be used\n", argv[0]);
       break;
     case 'L':
       offlinerc = optarg;
@@ -98,20 +98,46 @@ int main(int argc, char** argv) {
     case 'V':
       parm_list_file = optarg;
       break;
+    case 'v':
+      ++verbose;
+      break;
     default:
       break;
     }
   }
 
-  if (platform == -1UL || !partition || !offlinerc || !experiment_name) {
+  if (platform == -1UL || !partition || !offlinerc) {
     fprintf(stderr, "Missing parameters!\n");
     usage(argv[0]);
     return 1;
   }
 
-  offlineclient = new OfflineClient(offlinerc, partition, experiment_name);
+  // Parse instrument name for optional station number.
+  // Examples: "AMO", "SXR:0", "CXI:0", "CXI:1" 
+  char instr[64];
+  unsigned station = 0u;
+  char *pColon;
+  strncpy(instr, partition, sizeof(instr));
+  pColon = index(instr, ':');
+  if (pColon) {
+    *pColon++ = '\0';
+    if (sscanf(pColon, "%u", &station) != 1) {
+      fprintf(stderr, "%s: Error parsing instrument name '%s'\n", argv[0], partition);
+      usage(argv[0]);
+      return 1;
+    }
+  }
+  offlineclient = new OfflineClient(offlinerc, instr, station, (verbose > 0));
 
-  app = new OfflineAppliance(offlineclient, parm_list_file);
+  const char *expname = offlineclient->GetExperimentName();
+  if (expname) {
+    printf("%s: instrument %s:%u experiment %s (#%u)\n", argv[0],
+           instr, station, expname, offlineclient->GetExperimentNumber());
+    app = new OfflineAppliance(offlineclient, parm_list_file);
+  } else {
+    fprintf(stderr, "%s: failed to find current experiment\n", argv[0]);
+    app = NULL;
+  }
   if (app) {
     Task* task = new Task(Task::MakeThisATask);
     MyCallback* display = new MyCallback(task, app);
@@ -122,18 +148,21 @@ int main(int argc, char** argv) {
                *display);
 
     if (event->attach()) {
+      if (verbose) {
+        printf("%s: Observer attached, entering main loop...\n", argv[0]);
+      }
       task->mainLoop();
       event->detach();
       delete app;
       delete event;
       delete display;
     } else {
-      printf("Observer failed to attach to platform\n");
+      fprintf(stderr, "%s: Observer failed to attach to platform\n", argv[0]);
       delete app;
       delete event;
     }
   } else {
-    printf("%s: Error creating OfflineAppliance\n", __FUNCTION__);
+    fprintf(stderr, "%s: Error creating OfflineAppliance\n", argv[0]);
   }
   return 0;
 }
