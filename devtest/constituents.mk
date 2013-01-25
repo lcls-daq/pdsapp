c992dd2378faa528e2078b558a6f458a034eb9fa
weaver <none@example.com>
2013-01-25 23:57:00 +0000

    Added dsstest for testing DSS node disk performance

--- devtest/constituents.mk
+++ devtest/constituents.mk
@@ -1,35 +1,39 @@
 CPPFLAGS += -D_ACQIRIS -D_LINUX
 
 #tgtnames    := evgr xtcwriter pnccd xtctruncate pnccdreader
-tgtnames    := evgr pnccd xtctruncate pnccdreader
+tgtnames    := evgr pnccd xtctruncate pnccdreader dsstest
 
 tgtsrcs_evrobs := evrobs.cc
 tgtincs_evrobs := evgr
 tgtlibs_evrobs := pdsdata/xtcdata evgr/evr evgr/evg pds/service pds/collection pds/xtc pds/utility pds/management pds/client pds/evgr 
-tgtslib_evrobs := /usr/lib/rt
+tgtslib_evrobs := $(USRLIB)/rt
 
 tgtsrcs_evgr := evgr.cc
 tgtincs_evgr := evgr
 tgtlibs_evgr := pdsdata/xtcdata pdsdata/evrdata evgr/evg evgr/evr pds/service pds/collection pds/config pds/xtc pds/mon pds/vmon pds/utility pds/management pds/client pds/evgr 
-tgtslib_evgr := /usr/lib/rt
+tgtslib_evgr := $(USRLIB)/rt
 
 tgtsrcs_evgrd := evgrd.cc
 tgtincs_evgrd := evgr
 tgtlibs_evgrd := pdsdata/xtcdata pdsdata/evrdata evgr/evg evgr/evr pds/service pds/collection pds/config pds/xtc pds/mon pds/vmon pds/utility pds/management pds/client pds/evgr
-tgtslib_evgrd := /usr/lib/rt
+tgtslib_evgrd := $(USRLIB)/rt
 
 tgtsrcs_xtcwriter := xtcwriter.cc
 tgtlibs_xtcwriter := pdsdata/xtcdata pdsdata/acqdata pds/service pds/xtc pds/collection pds/mon pds/vmon pds/utility
-tgtslib_xtcwriter := /usr/lib/rt
+tgtslib_xtcwriter := $(USRLIB)/rt
 
 tgtsrcs_pnccdreader := pnccdreader.cc
 tgtlibs_pnccdreader := pdsdata/pnccddata pdsdata/xtcdata pds/service
-tgtslib_pnccdreader := /usr/lib/rt
+tgtslib_pnccdreader := $(USRLIB)/rt
 
 tgtsrcs_pnccd := pnccd.cc
 tgtlibs_pnccd := pdsdata/xtcdata pds/service
-tgtslib_pnccd := /usr/lib/rt
+tgtslib_pnccd := $(USRLIB)/rt
 
 tgtsrcs_xtctruncate := xtctruncate.cc
 tgtlibs_xtctruncate := pdsdata/xtcdata pds/service
-tgtslib_xtctruncate := /usr/lib/rt
+tgtslib_xtctruncate := $(USRLIB)/rt
+
+tgtsrcs_dsstest := dsstest.cc
+tgtlibs_dsstest := pds/service
+tgtslib_dsstest := $(USRLIB)/rt
