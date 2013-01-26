3ffe7811d727f9518496b6670798fbdf330c6d2a
jackp <none@example.com>
2013-01-26 00:13:24 +0000

    Fri Jan 25 16:13:06 PST 2013 jackp make the 64 bit pdsapp/tools

--- packages.mk
+++ packages.mk
@@ -4,7 +4,7 @@ packages := tools config dev devtest mon monobs control epics blv bldIpimb test 
 endif
 
 ifneq ($(findstring x86_64-linux,$(tgt_arch)),)
-packages := config dev python blv bldIpimb test camrecord
+packages := tools config dev python blv bldIpimb test camrecord
 endif
 
 ifneq ($(findstring ppc-rtems-rce,$(tgt_arch)),)
