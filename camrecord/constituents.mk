2c8ab5aeace44f991c7bb3e0a0f991127c702b8f
mcbrowne <none@example.com>
2013-01-30 18:52:04 +0000

    Added xtcmerge utility.

--- camrecord/constituents.mk
+++ camrecord/constituents.mk
@@ -1,6 +1,9 @@
-tgtnames := camrecord
+tgtnames := camrecord xtcmerge
 
 commonlibs := pdsdata/xtcdata pdsdata/pulnixdata pdsdata/opal1kdata pdsdata/cspaddata pdsdata/cspad2x2data pdsdata/timepixdata pdsdata/camdata pdsdata/compressdata pdsdata/lusidata
 tgtsrcs_camrecord := bld.cc ca.cc main.cc xtc.cc
 tgtlibs_camrecord := $(commonlibs) epics/ca epics/Com
 tgtincs_camrecord := epics/include epics/include/os/Linux
+
+tgtsrcs_xtcmerge := xtcmerge.cc
+tgtlibs_xtcmerge := pdsdata/xtcdata
