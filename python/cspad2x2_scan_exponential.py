4f2f108590dab3846645208d82dd0b3dd93a620a
jackp <none@example.com>
2013-02-07 17:09:51 +0000

    Thu Feb  7 09:09:15 PST 2013 jackp Integrated the shutter operation into the scanning.

--- python/cspad2x2_scan_exponential.py
+++ python/cspad2x2_scan_exponential.py
@@ -3,6 +3,7 @@
 
 import pydaq
 import pycdb
+import serial
 
 from optparse import OptionParser
 
@@ -19,7 +20,7 @@ if __name__ == "__main__":
     parser.add_option("-t","--typeID",dest="typeID",type="int",default=0x2002b,
                       help="type ID to generate",metavar="TYPEID")
     parser.add_option("-P","--parameter",dest="parameter",type="string",
-                      help="cspad2x2 quad parameter to scan", metavar="PARAMETER")
+                      help="cspad2x2 parameter to scan", metavar="PARAMETER")
     parser.add_option("-s","--start",dest="start",type="int", default=2,
                       help="parameter start", metavar="START")
     parser.add_option("-m","--multiplier",dest="multiplier",type="float",nargs=1,default=2.0,
@@ -30,6 +31,8 @@ if __name__ == "__main__":
                       help="record N events/cycle", metavar="N")
     parser.add_option("-l","--limit",dest="limit",type="int",default=99,
                       help="limit number of configs to less than number of steps", metavar="N")
+    parser.add_option("-S","--shutter",dest="shutter",default="None",
+                      help="path to shutter serial port", metavar="SHUTTER")
     (options, args) = parser.parse_args()
     
     print 'host', options.host
@@ -41,7 +44,10 @@ if __name__ == "__main__":
     print 'events', options.events
     print 'detector', hex(options.detector)
     print 'typeID', hex(options.typeID)
-    if options.steps<options.limit : options.limit = options.steps
+    print 'shutter', options.shutter
+    if options.shutter != 'None' and options.limit > 49 :
+	options.limit = 49
+    if options.steps < options.limit : options.limit = options.steps
     else : print 'Warning, range will be covered in', options.limit, \
          'but will still do', options.steps, 'steps with wrapping'
     daq = pydaq.Control(options.host,options.platform)
@@ -64,41 +70,42 @@ if __name__ == "__main__":
     xtc = cdb.get(key=key,src=options.detector,typeid=options.typeID)[0]
     print 'xtc is', xtc
     cspad = xtc.get(0)
-    foundParameter = 'FALSE'
-    parameterType = 'none'
-    for member in cspad['quad'][0] :
-        if member == options.parameter :
-            print 'Found the', options.parameter, 'quad parameter'
-            foundParameter = 'TRUE'
-            parameterType = 'quad'
+    parameterType = 'None'
     for member in cspad :
         if member == options.parameter :
             print 'Found the', options.parameter, 'concentrator parameter'
-            foundParameter = 'TRUE'
             parameterType = 'concentrator'
-    if foundParameter == 'FALSE' :
+    for member in cspad['quad'][0] :
+        if member == options.parameter :
+            print 'Found the', options.parameter, 'quad parameter'
+            parameterType = 'quad'
+    if parameterType == 'None' :
         print 'Parameter', options.parameter, 'not found!'
         print '    Allowed concentrator parameters : current values'
         for member in cspad :
             if member!='quad' and not member.endswith('System') and not member.endswith('Version') and not member.endswith('Mask') and not member.endswith('Enable') : 
-                print '        ', member, cspad[member]
+                print '        ', member, ':', cspad[member]
         print '    Allowed quad parameters : current values'
         for member in cspad['quad'][0] :
             if member!='gain' and member!='pots' and not member.endswith('Select') :
 		print '        ', member, ':',   cspad['quad'][0][member]
-#    print 'cspad inactiveRunMode =', cspad['inactiveRunMode'], 'activeRunMode =', cspad['activeRunMode'], 'payloadSize =', cspad['payloadSize']
-    if foundParameter == 'TRUE' :
-        print 'Now we will start scanning ...'
-        value = options.start
-        for cycle in range(options.limit+1):
+    else :
+        print 'Composing the sequence of configurations ...'
+        value = float(options.start)
+        cycleLength = 1
+	shutterActive = options.shutter != 'None'
+        if shutterActive :
+	    cycleLength = 2
+        for cycle in range(options.limit*cycleLength+1):
             if parameterType == 'quad' :
-                cspad['quad'][0][options.parameter]=value
+                cspad['quad'][0][options.parameter]=int(value)
             if parameterType == 'concentrator' :
-		cspad[options.parameter]=value
+		cspad[options.parameter]=int(value)
             xtc.set(cspad,cycle)
-	    value = int(value * options.multiplier)
+	    if cycle % cycleLength or not shutterActive :
+	        value = value * options.multiplier
         cdb.substitute(newkey,xtc)
-        print 'done'
+        print '    done'
 #
 #  Could scan EVR simultaneously
 #
@@ -114,6 +121,11 @@ if __name__ == "__main__":
                       controls=[(options.parameter,options.start)])
         print "Configured."
 
+# set up shutter
+        if shutterActive :
+            ser = serial.Serial(options.shutter)
+            ser.write(chr(129)) ## close shutter
+
 #
 #  Wait for the user to declare 'ready'
 #    Setting up monitoring displays for example
@@ -122,20 +134,29 @@ if __name__ == "__main__":
 
         for cycle in range(options.steps):
             if cycle%(options.limit+1) == 0 : value = options.start
-            print "Cycle", cycle, "-", options.parameter, "=", value
-            daq.begin(controls=[(options.parameter,value)])
+	    if shutterActive :
+		ser.write(chr(129)) ## close shutter
+		print "Cycle", cycle, " closed -", options.parameter, "=", int(value)
+            else :
+		print "Cycle", cycle, "-", options.parameter, "=", int(value)
+            daq.begin(controls=[(options.parameter,int(value))])
             # wait for enabled , then enable the EVR sequence
 
             # wait for disabled, then disable the EVR sequence
-            daq.end()  
-    
-	    value = int(value * options.multiplier)
+            daq.end() 
+    	    if shutterActive :
+                print "        opened -", options.parameter, "=", int(value)
+                ser.write(chr(128)) ## open shutter
+                daq.begin(controls=[(options.parameter,int(value))])
+                daq.end()
+                ser.write(chr(129)) ## close shutter
+		
+	    value = value * options.multiplier
         
 #
 #  Wait for the user to declare 'done'
 #    Saving monitoring displays for example
 #
-        ready = raw_input('--Hit Enter when Done-->')
-
-
-    
+	ready = raw_input('-- Finished, hit Enter to exit -->')
+        print 'Restoring key', hex(key)
+	daq.configure(key=key, events=1)
