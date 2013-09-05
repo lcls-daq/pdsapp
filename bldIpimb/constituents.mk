20578dc631abd18d0e58285e724e7322c901f63b
weaver <none@example.com>
2013-09-05 20:50:57 +0000

    Moved bldIpimbClient from ami to pdsapp

--- bldIpimb/constituents.mk
+++ bldIpimb/constituents.mk
@@ -4,7 +4,7 @@
 
 libsrcs_bldIpimblib := BldIpimbStream.cc  ToBldEventWire.cc EvrBldManager.cc EvrBldServer.cc
 libincs_bldIpimblib := evgr pdsdata/include ndarray/include 
-tgtnames    := bldIpimb
+tgtnames    := bldIpimb bldipimbclient
 
 
 commonlibs  := pdsdata/xtcdata pdsdata/appdata
@@ -18,6 +18,8 @@ datalibs := pdsdata/xtcdata pdsdata/aliasdata pdsdata/psddl_pdsdata
 ARCHCODE=32
 #endif
 
+qt_libs := $(qtlibdir)
+
 tgtsrcs_bldIpimb := BldIpimb.cc
 tgtsrcs_bldIpimb += $(libsrcs_bldIpimblib)
 tgtincs_bldIpimb := evgr pdsdata/include ndarray/include 
@@ -26,4 +28,8 @@ tgtlibs_bldIpimb := $(commonlibs) $(datalibs) evgr/evr evgr/evg pds/evgr pds/ipi
 tgtlibs_bldIpimb += $(qtlibdir) pds/configdata
 tgtslib_bldIpimb := $(USRLIBDIR)/rt $(qtslibdir)
 
-
+tgtsrcs_bldipimbclient := bldIpimbClient.cc bldIpimbClient_moc.cc
+tgtlibs_bldipimbclient := $(datalibs) pdsapp/configdb pdsapp/configdbg pds/configdata
+tgtlibs_bldipimbclient += pdsalg/pdsalg
+tgtlibs_bldipimbclient += $(qt_libs)
+tgtincs_bldipimbclient := pdsdata/include $(qtincdir)
