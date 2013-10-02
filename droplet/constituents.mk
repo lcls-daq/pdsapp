9f0c491e53f45369d839aa0bbea528cf99aa233b
no author <no_author@nowhere.com>
2013-10-02 00:15:11 +0000

    This commit was manufactured by cvs2svn to create tag 'workshop_20131001'.

--- droplet/constituents.mk
+++
@@ -1,8 +0,0 @@
-tgtnames := droplet
-LXFLAGS += -Wl,--export-dynamic
-commonlibs := pdsdata/xtcdata pdsdata/pulnixdata pdsdata/opal1kdata pdsdata/cspaddata pdsdata/cspad2x2data pdsdata/timepixdata pdsdata/camdata pdsdata/compressdata pdsdata/lusidata
-
-tgtsrcs_droplet := droplet.cc
-tgtlibs_droplet := $(commonlibs)
-tgtslib_droplet := dl
-tgtincs_droplet := epics/include epics/include/os/Linux
