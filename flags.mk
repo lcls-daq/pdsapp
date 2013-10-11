d73e0d1863ff68414f215a7e961dc3cd9c011f82
no author <no_author@nowhere.com>
2013-10-11 00:20:41 +0000

    This commit was manufactured by cvs2svn to create tag 'V07-00-03'.

--- flags.mk
+++
@@ -1,9 +0,0 @@
-include $(RELEASE_DIR)/make/sw/flags.mk
-include $(RELEASE_DIR)/make/sw/qtflags.mk
-
-false :=
-true  := t
-#build_extra := $(true)
-build_extra := $(false)
-
-CPPFLAGS += -DPsddlPds=Pds
