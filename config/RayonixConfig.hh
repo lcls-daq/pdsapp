27d73f3f04bc2da7474bb13eb82bfb0dd707e736
weaver <none@example.com>
2013-12-19 22:09:02 +0000

    *** empty log message ***

--- config/RayonixConfig.hh
+++ config/RayonixConfig.hh
@@ -7,8 +7,25 @@ class QCheckBox;
 
 namespace Pds_ConfigDb
 {
+  class RayonixExpertConfig;
   class RayonixConfig;
 }
+
+class Pds_ConfigDb::RayonixExpertConfig
+  : public Serializer
+{
+  public:
+    RayonixExpertConfig();
+    ~RayonixExpertConfig() {}
+
+    int  readParameters (void* from);
+    int  writeParameters(void* to);
+    int  dataSize       () const;
+
+  private:
+    class Private_Data;
+    Private_Data* _private_data;
+};
 
 class Pds_ConfigDb::RayonixConfig
   : public Serializer
