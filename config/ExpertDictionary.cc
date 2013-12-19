79f1c37b97595d894dcace5e07585646a707cf8e
jana <none@example.com>
2013-12-19 20:13:51 +0000

    Support for ConfigV2 and Expert Mode

--- config/ExpertDictionary.cc
+++ config/ExpertDictionary.cc
@@ -2,6 +2,7 @@
 
 #include "pdsapp/config/EvrConfig.hh"
 #include "pdsapp/config/TimepixConfig.hh"
+#include "pdsapp/config/RayonixConfig.hh"
 
 #ifdef BUILD_EXTRA
 #include "pdsapp/config/PhasicsConfig.hh"
@@ -9,6 +10,7 @@
 
 #include "pds/config/EvrConfigType.hh"
 #include "pds/config/TimepixConfigType.hh"
+#include "pds/config/RayonixConfigType.hh"
 
 using namespace Pds_ConfigDb;
 
@@ -25,6 +27,7 @@ Serializer* ExpertDictionary::lookup(const Pds::TypeId& type)
 #define enroll(_type, v) { if (type.value()==_type.value()) return v; }
   enroll(_evrConfigType   , new EvrConfig);
   enroll(_timepixConfigType   ,new TimepixExpertConfig);
+  enroll(_rayonixConfigType,   new RayonixExpertConfig);
 #ifdef BUILD_EXTRA
   enroll(_PhasicsConfigType   ,new PhasicsExpertConfig);
 #endif
