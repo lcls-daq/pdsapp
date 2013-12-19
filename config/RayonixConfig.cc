b549bfc9950cb67c92a641379d81f7c2aa1b84ef
jana <none@example.com>
2013-12-19 21:41:21 +0000

    Added support for Rayonix ConfigV2 and Expert mode

--- config/RayonixConfig.cc
+++ config/RayonixConfig.cc
@@ -9,11 +9,142 @@
 #include <new>
 
 namespace Pds_ConfigDb {
+   // these must end in NULL
+   static const char* full_readoutMode_to_name[] = { "Standard", "High Gain", "Low Noise", "HDR", NULL };
+   static const char* full_res_to_name[] = { "3840x3840 (binning 1x1)",
+                                             "1920x1920 (binning 2x2)", 
+                                             "1280x1280 (binning 3x3)", 
+                                             "960x960 (binning 4x4)", 
+                                             "768x768 (binning 5x5)", 
+                                             "640x640 (binning 6x6)", 
+                                             "480x480 (binning 8x8)", 
+                                             "384x384 (binning 10x10)",
+                                             NULL };
+
 
   // these must end in NULL
   static const char* readoutMode_to_name[] = { "Standard", "High Gain", "Low Noise", "EDR", NULL };
   static const char* res_to_name[] = { "1920x1920 (binning 2x2)", "960x960 (binning 4x4)", NULL };
 
+  // ------- expert mode ---------
+  class RayonixExpertConfig::Private_Data {
+  public:
+    enum Resolution { res3840x3840=0, 
+                      res1920x1920=1, 
+                      res1280x1280=2, 
+                      res960x960=3,
+                      res768x768=4, 
+                      res640x640=5, 
+                      res480x480=6, 
+                      res384x384=7}; 
+
+
+    Private_Data() :
+      _readoutMode    ("Readout mode", RayonixConfigType::Standard, full_readoutMode_to_name),
+      _resolution     ("Resolution",      res1920x1920,           full_res_to_name),
+      _binning_f      (0),    // derived from _resolution
+      _binning_s      (0),    // derived from _resolution
+      _testPattern    ("Test pattern",          0,            -1,32767),
+      _exposure_ms    ("Exposure (ms)",         0,            0,10000),
+      _trigger        ("Trigger (hex)",         0,            0,0xffffffff, Hex),
+      _rawMode        ("Raw mode (0 or 1)",     0,            0,1),
+      _darkFlag       ("Dark flag (0 or 1)",    0,            0,1)
+    {}
+
+    void insert(Pds::LinkedList<Parameter>& pList) {
+      pList.insert(&_readoutMode);
+      pList.insert(&_resolution);
+      pList.insert(&_exposure_ms);
+      pList.insert(&_testPattern);
+      pList.insert(&_trigger);
+      pList.insert(&_rawMode);
+      pList.insert(&_darkFlag);
+    }
+
+    uint32_t binningEnum2int(uint32_t in)
+    {
+      switch (in) {
+        case res3840x3840: return(1);
+        case res1920x1920: return (2);
+        case res1280x1280: return(3); 
+        case res960x960: return (4);
+        case res768x768: return(5);
+        case res640x640: return (6);
+        case res480x480: return(8);
+        case res384x384: return(10);
+        default: break;
+      }
+      return (2);
+    }
+
+    uint32_t binning2enum(uint32_t in)
+    {
+      switch (in) {
+        case 1: return(res3840x3840);
+        case 2: return(res1920x1920);
+        case 3: return(res1280x1280); 
+        case 4: return(res960x960);
+        case 5: return(res768x768);
+        case 6: return(res640x640);
+        case 8: return(res480x480);
+        case 10: return(res384x384);
+        default: break;
+      }
+      return (2);
+    }
+
+    int pull(void* from) {
+      RayonixConfigType& tc = *reinterpret_cast<RayonixConfigType*>(from);
+
+      _binning_f = tc.binning_f();
+      _binning_s = tc.binning_s();
+      _resolution.value = binning2enum(_binning_f);
+      _testPattern.value = tc.testPattern();
+      _exposure_ms.value = tc.exposure();
+      _trigger.value = tc.trigger();
+      _rawMode.value = tc.rawMode();
+      _darkFlag.value = tc.darkFlag();
+      _readoutMode.value = tc.readoutMode();
+      return tc._sizeof();
+    }
+
+    int push(void* to) {
+      // restriction: binning_f == binning_s
+      _binning_f = _binning_s = binningEnum2int(_resolution.value);
+
+      RayonixConfigType& tc = *new(to) RayonixConfigType(
+        _binning_f,
+        _binning_s,
+        _testPattern.value,
+        _exposure_ms.value,
+        _trigger.value,
+        _rawMode.value,
+        _darkFlag.value,
+        _readoutMode.value,
+        "name:0000"
+        );
+
+      return tc._sizeof();
+    }
+
+    int dataSize() const {
+      return sizeof(RayonixConfigType);
+    }
+
+  public:
+    Enumerated<RayonixConfigType::ReadoutMode> _readoutMode;
+    Enumerated<uint32_t> _resolution;
+    uint8_t _binning_f;
+    uint8_t _binning_s;
+    NumericInt<int16_t> _testPattern;
+    NumericInt<uint16_t> _exposure_ms;
+    NumericInt<uint32_t> _trigger;
+    NumericInt<uint16_t> _rawMode;
+    NumericInt<uint16_t> _darkFlag;
+  };
+
+
+  // ----------- user mode --------
   class RayonixConfig::Private_Data {
   public:
     enum Resolution { res1920x1920=0, res960x960=1 }; 
@@ -102,6 +233,28 @@ namespace Pds_ConfigDb {
 };
 
 using namespace Pds_ConfigDb;
+
+// ------------ expert mode ------------
+RayonixExpertConfig::RayonixExpertConfig() :
+  Serializer("Rayonix_Expert Config"),
+  _private_data( new Private_Data )
+{
+  _private_data->insert(pList);
+}
+
+int  RayonixExpertConfig::readParameters (void* from) {
+  return _private_data->pull(from);
+}
+
+int  RayonixExpertConfig::writeParameters(void* to) {
+  return _private_data->push(to);
+}
+
+int  RayonixExpertConfig::dataSize() const {
+  return _private_data->dataSize();
+}
+
+// ------------ user mode ------------
 
 RayonixConfig::RayonixConfig() :
   Serializer("Rayonix_Config"),
