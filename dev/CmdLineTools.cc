6edb5f2bb5be448463b0b94f0afc4398e2b060cb
weaver <none@example.com>
2014-07-28 16:55:41 +0000

    Add command-line parsing tools to pdsapp/dev/CmdLineTools

--- dev/CmdLineTools.cc
+++ dev/CmdLineTools.cc
@@ -66,3 +66,40 @@ bool CmdLineTools::parseDetInfo(const char* args, DetInfo& info)
   printf("Sourcing %s\n",DetInfo::name(info));
   return true;
 }
+
+
+bool CmdLineTools::parseInt   (const char* arg, int& v)
+{
+  char* endptr;
+  v = strtol(arg,&endptr,0);
+  return *endptr==0;
+}
+
+bool CmdLineTools::parseUInt  (const char* arg, unsigned& v)
+{
+  char* endptr;
+  v = strtoul(arg,&endptr,0);
+  return *endptr==0;
+}
+
+bool CmdLineTools::parseUInt64(const char* arg, uint64_t& v)
+{
+  char* endptr;
+  v = strtoull(arg,&endptr,0);
+  return *endptr==0;
+}
+
+bool CmdLineTools::parseFloat (const char* arg, float& v)
+{
+  char* endptr;
+  v = strtof(arg,&endptr);
+  return *endptr==0;
+}
+
+bool CmdLineTools::parseDouble(const char* arg, double& v)
+{
+  char* endptr;
+  v = strtod(arg,&endptr);
+  return *endptr==0;
+}
+
--- dev/CmdLineTools.hh
+++ dev/CmdLineTools.hh
@@ -1,11 +1,18 @@
 #ifndef PdsApp_CmdLineTools_hh
 #define PdsApp_CmdLineTools_hh
 
+#include <stdint.h>
+
 namespace Pds {
   class DetInfo;
   class CmdLineTools {
   public:
     static bool parseDetInfo(const char* args, DetInfo& info);
+    static bool parseInt   (const char* arg, int&);
+    static bool parseUInt  (const char* arg, unsigned&);
+    static bool parseUInt64(const char* arg, uint64_t&);
+    static bool parseFloat (const char* arg, float&);
+    static bool parseDouble(const char* arg, double&);
   };
 };
 
