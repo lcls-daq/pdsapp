9e2e158a0ad0c3b81e7dc9a2473a7d387bd57a5e
caf <caf@SLAC.STANFORD.EDU>
2014-08-28 00:43:05 +0000

    institute stricter error checking

--- tools/showPartitions.cc
+++ tools/showPartitions.cc
@@ -83,8 +83,12 @@ private:
 
 #include <time.h>
 
-int main()
+int main(int argc, char **argv)
 {
+  if (argc > 1) {
+    printf("Usage: %s [-h]\n", argv[0]);
+    exit(1);
+  }
   Observer o;
   o.start();
   if (o.connect()) {
