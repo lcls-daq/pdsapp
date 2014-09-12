e151c06ca494c4be11f82ae1d60a995a6ab3d095
caf <caf@SLAC.STANFORD.EDU>
2014-09-12 19:35:39 +0000

    institute stricter error checking

--- dev/usdusb.cc
+++ dev/usdusb.cc
@@ -220,8 +220,9 @@ int main(int argc, char** argv) {
       }
       break;
     case 'u':
-      if (strlen(optarg) > SrcAlias::AliasNameMax-1) {
-        printf("Device alias '%s' exceeds %d chars, ignored\n", optarg, SrcAlias::AliasNameMax-1);
+      if (!CmdLineTools::parseSrcAlias(optarg)) {
+        printf("%s: option `-u' parsing error\n", argv[0]);
+        lUsage = true;
       } else {
         uniqueid = optarg;
       }
