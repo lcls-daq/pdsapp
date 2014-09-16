01c57a0ff46375cf8d500c4ab91a692f0f93f110
caf <caf@SLAC.STANFORD.EDU>
2014-09-16 01:21:02 +0000

    add module and channel, and institute stricter error checking

--- dev/andor.cc
+++ dev/andor.cc
@@ -7,6 +7,7 @@
 #include "pdsdata/xtc/DetInfo.hh"
 #include "pdsdata/psddl/epics.ddl.h"
 
+#include "pds/service/CmdLineTools.hh"
 #include "pds/management/SegmentLevel.hh"
 #include "pds/management/EventCallback.hh"
 #include "pds/collection/Arp.hh"
@@ -22,6 +23,8 @@
 #include "pds/andor/AndorManager.hh"
 #include "pds/andor/AndorServer.hh"
 
+extern int optind;
+
 using std::string;
 
 namespace Pds
@@ -31,7 +34,14 @@ static const char sAndorVersion[] = "0.9";
 class SegWireSettingsAndor : public SegWireSettings
 {
 public:
-    SegWireSettingsAndor(const Src& src, string sAliasName)
+    SegWireSettingsAndor(const Src& src,
+                       bool       bTriggered,
+                       unsigned   uModule,
+                       unsigned   uChannel,
+                       string     sAliasName) :
+      _bTriggered (bTriggered),
+      _uModule    (uModule),
+      _uChannel   (uChannel)
     {
       _sources.push_back(src);
       if (sAliasName.length())
@@ -47,9 +57,16 @@ public:
     {
       return (_aliases.size() > 0) ? &_aliases : NULL;
     }
+    bool     is_triggered() const { return _bTriggered; }
+    unsigned module      () const { return _uModule; }
+    unsigned channel     () const { return _uChannel; }
+
 private:
     std::list<Src>      _sources;
     std::list<SrcAlias> _aliases;
+    bool                _bTriggered;
+    unsigned            _uModule;
+    unsigned            _uChannel;
 };
 
 //
@@ -156,19 +173,19 @@ static void showUsage()
 {
     printf( "Usage:  andor  [-v|--version] [-h|--help] [-c|--camera <0-9> ] "
       "[-i|--id <id>] [-d|--delay] [-n|--init] [-g|--config <db_path>] [-s|--sleep <ms>] "
-      "[-l|--debug <level>] [-u|--uniqueid <alias>] -p|--platform <platform id>\n"
+      "[-l|--debug <level>] [-u|--uniqueid <alias>] -p|--platform <platform>[,<mod>,<chan>]\n"
       "  Options:\n"
-      "    -v|--version                 Show file version.\n"
-      "    -h|--help                    Show usage.\n"
-      "    -c|--camera   [0-9]          Select the andor device. (Default: 0)\n"
-      "    -i|--id       <id>           Set ID. Format: Detector/DetectorId/DeviceId. (Default: NoDetector/0/0)\n"
-      "    -u|--uniqueid <alias>        Set device alias.\n"
-      "    -d|--delay                   Use delay mode.\n"
-      "    -n|--init                    Run a testing capture to avoid the initial delay.\n"
-      "    -g|--config   <db_path>      Intial andor camera based on the config db at <db_path>\n"
-      "    -s|--sleep    <sleep_ms>     Sleep inteval between multiple perinceton camera. (Default: 0 ms)\n"
-      "    -l|--debug    <level>        Set debug level. (Default: 0)\n"
-      "    -p|--platform <platform id>  [*required*] Set platform id.\n"
+      "    -v|--version                             Show file version.\n"
+      "    -h|--help                                Show usage.\n"
+      "    -c|--camera   [0-9]                      Select the andor device. (Default: 0)\n"
+      "    -i|--id       <id>                       Set ID. Format: Detector/DetectorId/DeviceId. (Default: NoDetector/0/0)\n"
+      "    -u|--uniqueid <alias>                    Set device alias.\n"
+      "    -d|--delay                               Use delay mode.\n"
+      "    -n|--init                                Run a testing capture to avoid the initial delay.\n"
+      "    -g|--config   <db_path>                  Initialize andor camera based on the config db at <db_path>\n"
+      "    -s|--sleep    <sleep_ms>                 Sleep interval between multiple andor cameras. (Default: 0 ms)\n"
+      "    -l|--debug    <level>                    Set debug level. (Default: 0)\n"
+      "    -p|--platform <platform>[,<mod>,<chan>]  Set platform id [*required*], EVR module, EVR channel\n"
     );
 }
 
@@ -217,8 +234,13 @@ int main(int argc, char** argv)
     bool              bInitTest     = false;
     int               iDebugLevel   = 0;
     int               iPlatform     = -1;
+    unsigned          uModule       = 0;
+    unsigned          uChannel      = 0;
     string            sConfigDb;
     int               iSleepInt     = 0; // 0 ms
+    bool              bShowUsage    = false;
+    bool              bTriggered    = false;
+    unsigned          uu1, uu2, uu3;
 
     int               iOptionIndex  = 0;
     while ( int opt = getopt_long(argc, argv, strOptions, loOptions, &iOptionIndex ) )
@@ -227,25 +249,55 @@ int main(int argc, char** argv)
 
         switch(opt)
         {
-        case 'v':               /* Print usage */
+        case 'v':               /* Print version */
             showVersion();
             return 0;
         case 'p':
-            iPlatform = strtoul(optarg, NULL, 0);
+            switch (CmdLineTools::parseUInt(optarg,uu1,uModule,uChannel))
+            {
+            case 1:
+              bTriggered = false;
+              break;
+            case 3:
+              bTriggered = true;
+              break;
+            default:
+              printf( "andor:main(): option `-p' parsing error\n" );
+              bShowUsage = true;
+              break;
+            }
+            iPlatform = (int) uu1;
             break;
         case 'c':
-            iCamera = strtoul(optarg, NULL, 0);
+            if (!CmdLineTools::parseInt(optarg, iCamera))
+            {
+              printf( "andor:main(): option `-c' parsing error\n" );
+              bShowUsage = true;
+            }
             break;
         case 'i':
-            char* pNextToken;
-            detector    = (DetInfo::Detector) strtoul(optarg, &pNextToken, 0); ++pNextToken;
-            if ( *pNextToken == 0 ) break;
-            iDetectorId = strtoul(pNextToken, &pNextToken, 0); ++pNextToken;
-            if ( *pNextToken == 0 ) break;
-            iDeviceId   = strtoul(pNextToken, &pNextToken, 0);
+            if (CmdLineTools::parseUInt(optarg,uu1,uu2,uu3,0,'/') != 3)
+            {
+              printf( "andor:main(): option `-i' parsing error\n" );
+              bShowUsage = true;
+            }
+            else
+            {
+              detector = (DetInfo::Detector) uu1;
+              iDetectorId = (int) uu2;
+              iDeviceId = (int) uu3;
+            }
             break;
        case 'u':
-            sUniqueId = optarg;
+            if (!CmdLineTools::parseSrcAlias(optarg))
+            {
+              printf( "andor:main(): option `-u' parsing error\n" );
+              bShowUsage = true;
+            }
+            else
+            {
+              sUniqueId = optarg;
+            }
             break;
         case 'd':
             bDelayMode = true;
@@ -254,22 +306,32 @@ int main(int argc, char** argv)
             bInitTest = true;
             break;
         case 'l':
-            iDebugLevel = strtoul(optarg, NULL, 0);
+            if (!CmdLineTools::parseInt(optarg,iDebugLevel))
+            {
+              printf( "andor:main(): option `-l' parsing error\n" );
+              bShowUsage = true;
+            }
             break;
         case 'g':
             sConfigDb = optarg;
             break;
         case 's':
-            iSleepInt = strtoul(optarg, NULL, 0);
+            if (!CmdLineTools::parseInt(optarg,iSleepInt))
+            {
+              printf( "andor:main(): option `-s' parsing error\n" );
+              bShowUsage = true;
+            }
             break;
         case '?':               /* Terse output mode */
-          if (optopt)
-            printf( "andor:main(): Unknown option: %c\n", optopt );
-          else
-            printf( "andor:main(): Unknown option: %s\n", argv[optind-1] );
+            if (optopt)
+              printf( "andor:main(): Unknown option: %c\n", optopt );
+            else
+              printf( "andor:main(): Unknown option: %s\n", argv[optind-1] );
+            bShowUsage = true;
             break;
         case ':':               /* Terse output mode */
             printf( "andor:main(): Missing argument for %c\n", optopt );
+            bShowUsage = true;
             break;
         default:
         case 'h':               /* Print usage */
@@ -278,12 +340,20 @@ int main(int argc, char** argv)
         }
     }
 
-    argc -= optind;
-    argv += optind;
-
     if ( iPlatform == -1 )
     {
         printf( "andor:main(): Please specify platform in command line options\n" );
+        bShowUsage = true;
+    }
+
+    if (optind < argc)
+    {
+        printf(" andor:main(): invalid argument -- %s\n", argv[optind] );
+        bShowUsage = true;
+    }
+
+    if (bShowUsage)
+    {
         showUsage();
         return 1;
     }
@@ -305,7 +375,10 @@ int main(int argc, char** argv)
     {
     printf("Settings:\n");
 
-    printf("  Platform: %d  Camera: %d\n", iPlatform, iCamera);
+    if (bTriggered)
+      printf("  Platform: %d  EVR module: %u  EVR channel: %u  Camera: %d\n", iPlatform, uModule, uChannel, iCamera);
+    else
+      printf("  Platform: %d  Camera: %d\n", iPlatform, iCamera);
 
     const DetInfo detInfo( getpid(), detector, iDetectorId, DetInfo::Andor, iDeviceId);
     printf("  DetInfo: %s  ConfigDb: %s  Sleep: %d ms\n", DetInfo::name(detInfo), sConfigDb.c_str(), iSleepInt );
@@ -318,7 +391,7 @@ int main(int argc, char** argv)
     taskMainThread = task;
 
     CfgClientNfs cfgService = CfgClientNfs(detInfo);
-    SegWireSettingsAndor settings(detInfo, sUniqueId);
+    SegWireSettingsAndor settings(detInfo, bTriggered, uModule, uChannel, sUniqueId);
 
     EventCallBackAndor  eventCallBackAndor(iPlatform, cfgService, iCamera, bDelayMode, bInitTest, sConfigDb, iSleepInt, iDebugLevel);
     SegmentLevel segmentLevel(iPlatform, settings, eventCallBackAndor, NULL);
