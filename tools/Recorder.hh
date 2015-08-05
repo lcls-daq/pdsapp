bcdbe7ab4a8d539fa121b6b87780fea9194a78dd
jana <jana@SLAC.STANFORD.EDU>
2015-08-05 17:54:24 +0000

    Add support to write small data index file

--- tools/Recorder.hh
+++ tools/Recorder.hh
@@ -14,7 +14,7 @@ class GenericPool;
 
 class Recorder : public Appliance {
 public:
-  Recorder(const char* fname, unsigned int sliceID, uint64_t chunkSize, bool delay_xfer, OfflineClient *offline, const char* expname);
+   Recorder(const char* fname, unsigned int sliceID, uint64_t chunkSize, bool delay_xfer, OfflineClient *offline, const char* expname, unsigned uSizeThreshold);  
   ~Recorder() {}
   virtual Transition* transitions(Transition*);
   virtual InDatagram* occurrences(InDatagram* in);
@@ -24,7 +24,9 @@ private:
   int _openOutputFile(bool verbose);
   int _postDataFileError();
   int _writeOutputFile(const void *ptr, size_t size, size_t nmemb);
+  int _writeSmallDataFile(const void *ptr, size_t size);
   int _flushOutputFile();
+  int _flushSmallDataFile();
   int _closeOutputFile();
   int _renameOutputFile(bool verbose);
   int _renameOutputFile(int run, bool verbose);
@@ -32,21 +34,26 @@ private:
   int _requestChunk();
 
   FILE* _f;
+  FILE* _sdf;
   Pool* _pool;
   enum { SizeofPath=128 };
   char     _path[SizeofPath];
   enum { SizeofConfig=0x800000 };
   char     _config[SizeofConfig];
+  char     _smlconfig[SizeofConfig];
   Src      _src;
   unsigned _node;
   unsigned int _sliceID;
   unsigned _beginrunerr;
   bool     _path_error;
   bool     _write_error;
+  bool     _sdf_write_error;
   bool     _chunk_requested;
   enum { SizeofName=512 };
   char     _fname[SizeofName];
   char     _fnamerunning[SizeofName];
+  char     _sdfname[SizeofName];
+  char     _sdfnamerunning[SizeofName];
   unsigned int _chunk;
   uint64_t _chunkSize;
   bool     _delay_xfer;
@@ -59,6 +66,7 @@ private:
   char     _host_name[SizeofName];
   OfflineClient *_offlineclient;
   bool     _open_data_file_error;
+  unsigned _uSizeThreshold;
 };
 
 }
