af5c293e413e21b2c2b3b3a7121d734fb05876b3
jana <jana@SLAC.STANFORD.EDU>
2015-12-18 23:40:06 +0000

    Modify packages.mk to make rhel7 release that includes DAQ control level and event levels, but not (necessarily) segment levels

--- packages.mk
+++ packages.mk
@@ -1,13 +1,18 @@
 # List of packages (low level first)
 ifneq ($(findstring i386-linux,$(tgt_arch)),)
-packages := tools config dev devtest monobs control epics blv bldIpimb test camrecord
+packages := tools config dev devtest monobs control epics blv bldIpimb test camrecord1
 endif
 
 ifneq ($(findstring x86_64,$(tgt_arch)),)
 packages := tools config dev devtest mon monobs control python epics blv bldIpimb test camrecord
 endif
 
+ifneq ($(findstring x86_64-rhel7,$(tgt_arch)),)
+packages := tools config mon monobs control epics python blv bldIpimb test
+endif
+
 ifneq ($(findstring ppc-rtems-rce,$(tgt_arch)),)
 # List of packages (low level first)
 packages := 
 endif
+
