49d6bb719cf40438a6d0457365635c1e43590730
ddamiani <ddamiani@SLAC.STANFORD.EDU>
2017-10-30 17:33:35 +0000

    fix to build python 2 and 3 at the same time

--- python/constituents.mk
+++ python/constituents.mk
@@ -1,7 +1,7 @@
 libnames := pycdb pydaq py3cdb py3daq
 
 libsrcs_pycdb := pycdb.cc
-libincs_pycdb := python/include/python2.5 
+libincs_pycdb := python/include/python2.5
 libincs_pycdb += pdsdata/include ndarray/include boost/include  
 liblibs_pycdb := pdsdata/xtcdata pdsdata/psddl_pdsdata
 liblibs_pycdb += pds/service pds/utility pds/xtc pds/collection pds/vmon pds/mon
@@ -10,7 +10,7 @@ liblibs_pycdb += pds/configdbc pds/configsql pds/confignfs offlinedb/mysqlclient
 liblibs_pycdb += pdsapp/configdb
 libslib_pycdb := $(USRLIBDIR)/rt
 
-libsrcs_py3cdb := pycdb.cc
+libsrcs_py3cdb := py3cdb.cc
 libincs_py3cdb := python3/include/python3.6m
 libincs_py3cdb += pdsdata/include ndarray/include boost/include
 liblibs_py3cdb := pdsdata/xtcdata pdsdata/psddl_pdsdata
@@ -28,7 +28,7 @@ libincs_pydaq += pdsdata/include ndarray/include boost/include
 liblibs_pydaq += pdsdata/xtcdata pdsdata/psddl_pdsdata pds/configdata
 libslib_pydaq := $(USRLIBDIR)/rt
 
-libsrcs_py3daq := pydaq.cc
+libsrcs_py3daq := py3daq.cc
 libincs_py3daq := python3/include/python3.6m
 libincs_py3daq += pdsdata/include ndarray/include boost/include
 liblibs_py3daq += pdsdata/xtcdata pdsdata/psddl_pdsdata pds/configdata
---
+++ python/py3cdb.cc
@@ -0,0 +1,1 @@
+pycdb.cc
