2be22c15a50084129fdfb0a17467ae2d664557dc
Daniel Damiani <ddamiani@slac.stanford.edu>
2018-08-22 09:58:54 -0700

    add new wave8 bld for MEC

--- dev/bld.cc
+++ dev/bld.cc
@@ -248,7 +248,8 @@ namespace Pds {
     (ONE_BIT<<BldInfo::FeeAin01);
   BldBitMask BeamMonitorMask = (ONE_BIT<<BldInfo::MfxBeamMon01) |
     (ONE_BIT<<BldInfo::Hx2Sb1BeamMon) |
-    ((ONE_BIT<<(BldInfo::MfxDg2BeamMon+1)) - (ONE_BIT<<BldInfo::XcsSndDio));
+    ((ONE_BIT<<(BldInfo::MfxDg2BeamMon+1)) - (ONE_BIT<<BldInfo::XcsSndDio)) |
+    ((ONE_BIT<<(BldInfo::MecXt2BeamMon03+1)) - (ONE_BIT<<BldInfo::MecXt2BeamMon02));
   BldBitMask UsbEncoder = ((ONE_BIT<<(BldInfo::CxiUsbEncoder01+1)) - (ONE_BIT<<BldInfo::XrtUsbEncoder01));
 #define TEST_CREAT(mask, idType, dataType)                  \
   if ((im & mask).isNotZero()) {                            \
